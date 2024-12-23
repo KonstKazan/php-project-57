5ec8b9c1191ee239b2b745a51e89f6e0053508eb
Konstantin <konst.kazan@gmail.com>
2024-12-24 00:00:20 +0300

    change test

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,6 @@
         @if (Auth::check())
             <div class="flex items-center lg:order-2">
                 <a
-                    data-confirm="{{ __('other.confirmation') }}"
                     data-method="POST"
                     class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2"
                     href="{{ route('logout') }}">

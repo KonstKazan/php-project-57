7cbafc886394dcdc0e62ce113ec6d90a9fa82cd8
Konstantin <konst.kazan@gmail.com>
2025-01-02 14:17:45 +0300

    fix lint

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -32,11 +32,11 @@ class ProfileController extends Controller
 
         $user->fill($request->validated());
 
-        if ($request->user()->isDirty('email')) {
-            $request->user()->email_verified_at = null;
+        if ($user->isDirty('email')) {
+            $user->email_verified_at = null;
         }
 
-        $request->user()->save();
+        $user->save();
 
         return Redirect::route('profile.edit')->with('status', 'profile-updated');
     }
